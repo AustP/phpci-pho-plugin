09d2cf24ea11da8de5e0c17e6bd8dcd9d72c6ec6
Aust <austen@coremr.com>
2015-01-15 10:30:14 -0700

    v1.0.0
    Added README.md

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # phpci-pho-plugin
+This is a plugin for [PHPCI](https://github.com/block8/phpci). It allows you
+to run [pho](https://github.com/danielstjules/pho) tests via PHPCI.
+
+## Installation
+1. Navigate to your PHPCI path. `cd /path/to/phpci`
+2. Edit the composer.json file. `nano composer.json`
+3. Add `"austp\/phpci-pho-plugin": "~1.0"` in the `"require"` section.
+
+        "require": {
+            ...,
+            ...,
+            "austp\/phpci-pho-plugin": "~1.0"
+        }
+4. Download the plugin via composer. `composer update austp/phpci-pho-plugin`
+
+That's it as far as installation goes. Continue reading to see available options.
+
+
+## Configuration
+In order to configure PHPCI to run pho, you need to edit the `phpci.yml` file.
+If you don't already have this file in your repository, [go ahead and add it](https://www.phptesting.org/wiki/Adding-PHPCI-Support-to-Your-Projects).
+*Note: If you can't add a phpci.yml file to the repo, you can edit your project in PHPCI and configure it there.*
+
+### Options
+    directory:  "specs/"         | The directory to run the tests on.
+    log:        true             | (optional) Log pho's output to PHPCI.
+    executable: "/path/to/pho"   | (optional) Full path to a pho executable.
+    bootstrap:  "bootstrap.file" | (optional) Bootstrap file to load.
+    filter:     "filter"         | (optional) Run specs according to this filter.
+    namespace:  true             | (optional) Only use namespaced functions.
+
+### phpci.yml
+1. Navigate to your repository. `cd /path/to/repo`
+2. Edit the phpci.yml file. `nano phpci.yml`
+3. Add `\PHPCI_Pho_Plugin\Pho:` in the `"test"` section.
+
+        test:
+          ...:
+            ...: ...
+            ...: ...
+          ...:
+            ...: ...
+          \PHPCI_Pho_Plugin\Pho:
+4. Add your options under the `\PHPCI_Pho_Plugin\Pho:` line.
+
+        \PHPCI_Pho_Plugin\Pho:
+          directory: "specs/"
+          log: true
---
+++ src/Pho.php
@@ -0,0 +1,119 @@
+<?php
+/**
+* PHPCI - Continuous Integration for PHP
+*
+* @copyright    Copyright 2014, Block 8 Limited.
+* @license      https://github.com/Block8/PHPCI/blob/master/LICENSE.md
+* @link         https://www.phptesting.org/
+*/
+
+namespace PHPCI_Pho_Plugin;
+
+use PHPCI\Builder;
+use PHPCI\Model\Build;
+use PHPCI\Helper\Lang;
+use PHPCI\Plugin\Util\TapParser;
+
+/**
+* Pho plugin, runs Pho tests within a project.
+* @package PHPCI\Plugin
+*/
+class Pho implements \PHPCI\Plugin
+{
+  private $executable;
+  private $bootstrap;
+  private $filter;
+  private $namespace;
+  private $directory;
+  private $log;
+
+  /**
+  * Set up the plugin, configure options, etc.
+  * @param Builder $phpci
+  * @param Build $build
+  * @param array $options
+  */
+  public function __construct(Builder $phpci, Build $build, array $options = array())
+  {
+    $this->phpci = $phpci;
+    $this->build = $build;
+
+    if (isset($options['executable'])) {
+      $this->executable = $options['executable'];
+    } else {
+      $curdir = getcwd();
+      chdir($this->phpci->buildPath);
+      $this->executable = $this->phpci->findBinary('pho');
+      chdir($curdir);
+    }
+
+    if (isset($options['bootstrap'])) {
+      $this->bootstrap = $options['bootstrap'];
+    }
+    if (isset($options['filter'])) {
+      $this->filter = $options['filter'];
+    }
+    if (isset($options['namespace'])) {
+      $this->namespace = true;
+    }
+    if (isset($options['directory'])) {
+      $this->directory = $options['directory'];
+    }
+    if (isset($options['log'])) {
+      $this->log = true;
+    }
+  }
+
+  /**
+  * Run the Pho plugin.
+  * @return bool
+  */
+  public function execute()
+  {
+    if(!$this->executable) {
+      $this->phpci->logFailure(Lang::get('could_not_find', 'pho'));
+      return false;
+    }
+    if(!$this->directory) {
+      $this->phpci->logFailure(Lang::get('invalid_command'));
+      return false;
+    }
+
+    $this->phpci->logExecOutput(false);
+
+    $cmd = escapeshellarg($this->executable) . " -C --reporter spec";
+
+    if ($this->bootstrap) {
+      $bootstrapPath = $this->phpci->buildPath . $this->bootstrap;
+      $cmd .= " -b " . escapeshellarg($bootstrapPath);
+    }
+    if ($this->filter) {
+      $cmd .= " -f " . escapeshellarg($this->filter);
+    }
+    if ($this->namespace) {
+      $cmd .= " -n";
+    }
+
+    $cmd .= " " . escapeshellarg($this->phpci->buildPath . $this->directory);
+    $this->phpci->executeCommand($cmd);
+    $output = $this->phpci->getLastOutput();
+
+    if($this->log)
+      $this->phpci->log($output);
+
+    $matches = array();
+    preg_match('~(\d+) spec.*?(\d+) failure~', $output, $matches);
+    $specs = $matches[1];
+    $failures = $matches[2];
+
+    if ($specs == 0) {
+      $this->phpci->logFailure(Lang::get('no_tests_performed'));
+      return false;
+    } elseif ($failures > 0) {
+      $this->phpci->logFailure(implode(PHP_EOL, $output));
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
